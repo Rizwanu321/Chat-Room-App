50744274318bfef75419fc0002411a05520a9f44
Rizwanu321 <142315620+Rizwanu321@users.noreply.github.com>
2025-01-05 14:33:20 +0530

    Update socket.js

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://chat-room-app-tnj8.onrender.com", {
   withCredentials: true,
   transports: ["websocket", "polling"],
 });
